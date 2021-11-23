96f7d96814e7390f68d71160e6bc9ca467524b56
SOFOLITO <89881330+SOFOLITO@users.noreply.github.com>
2021-11-22 18:32:04 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var database;
+var position;
+var formulario;
+var player;
+var game;
+var canvas;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance =0;
+var car1, car2, car3, car4, cars;
+var track;
+var ground;
+var car1Img, car2Img, car3Img, car4Img;
+
+function preload(){
+track = loadImage("images/track.jpg");
+ground = loadImage("images/ground.png");
+car1Img = loadImage("images/car1.png");
+car2Img = loadImage("images/car2.png");
+car3Img = loadImage("images/car3.png");
+car4Img = loadImage("images/car4.png");
+
+}
+
+function setup(){
+ canvas = createCanvas(displayWidth - 20, displayHeight-30);
+ database = firebase.database();
+ game = new Game();
+ game.getState();
+ game.start();
+
+}
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+
+  }
+  if(gameState === 1){
+      clear();
+      game.play();
+      
+  }
+
+  if(gameState === 2){
+    game.end();
+  }
+}
